d2093996309d8c34d41f714eb77e33cb48871f16
anandrs <44742180+anandrs@users.noreply.github.com>
2018-11-05 23:31:29 +0530

    Create subtraction.js

---
+++ subtraction.js
@@ -0,0 +1,8 @@
+var num1=process.argv[2];
+      var num2=process.argv[3];
+      console.log('the subtraction of first two numbers are:'+(num1-num2));
+      
+      The ouput of the above program is:
+      ----------------------------------
+      C:\Users\ANAND\Operations>node sub.js 2 3
+      the subtraction of first two numbers are:-1
