40e437a3c621f584e0b3fa80003577c8f86fb3e3
Sripathi Anand Reddy <44742180+anandreddysripathi@users.noreply.github.com>
2018-11-06 07:17:00 +0530

    Create sort.js

---
+++ sort.js
@@ -0,0 +1,19 @@
+function Sort(arr){
+        var len = arr.length;
+        for (var i=len-1; i>=0; i--){
+          for(var j = 1; j<=i; j++){
+            if(Number(arr[j-1])>Number(arr[j])) {
+                var temp = Number(arr[j-1]);
+                arr[j-1] =Number(arr[j]);
+                arr[j] = temp;
+             }
+          }
+        }
+        return arr;
+     }
+     console.log(Sort(process.argv.slice(2)));
+     
+     The ouput of the above program is:
+     ----------------------------------
+     C:\Users\ANAND\Operations>node index.js 13 12 1 34 2 4
+     1,2,4,12,13,34
